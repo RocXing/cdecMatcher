bae0c4543a25c33223b6f94a19d698d2c6d48751
RocXing <120695313@qq.com>
2018-06-27 18:42:32 +0800

    主页面用户信息跳转

--- public/js/main.js
+++ public/js/main.js
@@ -5,10 +5,11 @@ function requestScoreAndFill() {
         let index = 0;
         for (let item of response.data) {
             let imgThumbnail = `?imageView2/2/w/32/h/32/interlace/1/q/100`;
+            let redirect = "./user.html?user_id=";
             let pic = "";
             if(index>=0 && index <4) pic = `<img src="http://olo9f0ooe.bkt.clouddn.com/${index+1}.png">`;
             let content = `<tr><td>${++index}${pic}</td>
-                            <td><img src="${item.head + imgThumbnail}">${item.name}</td>
+                            <td><a href="${redirect + item.id}"><img src="${item.head + imgThumbnail}">${item.name}</a></td>
                             <td>${item.description}</td>
                             <td>${item.score}</td>
                             <td>${item.times}</td>
