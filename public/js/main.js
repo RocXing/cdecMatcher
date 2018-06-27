7cf9d82c1e68c2d8524dc2b340be3e029423371f
RocXing <120695313@qq.com>
2018-06-27 15:15:36 +0800

    排名奖章

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,9 @@ function requestScoreAndFill() {
         let index = 0;
         for (let item of response.data) {
             let imgThumbnail = `?imageView2/2/w/32/h/32/interlace/1/q/100`;
-            let content = `<tr><td>${++index}</td>
+            let pic = "";
+            if(index>=0 && index <4) pic = `<img src="http://olo9f0ooe.bkt.clouddn.com/${index+1}.png">`;
+            let content = `<tr><td>${++index}${pic}</td>
                             <td><img src="${item.head + imgThumbnail}">${item.name}</td>
                             <td>${item.description}</td>
                             <td>${item.score}</td>
